378f8b0ef5f174230dabed16f9a6573bb79fe633
Enduxion <endux.pg@gmail.com>
2024-07-22 00:56:21 +0545

    Added smooth transition effect using animate presence

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   const routes = [
@@ -10,35 +11,43 @@ const App = () => {
       name: "home",
       path: "/",
       element: Home,
-      desc: "the main page"
+      desc: "the main page",
     },
     {
       name: "about",
       path: "/about",
       element: About,
-      desc: "a little about me"
+      desc: "a little about me",
     },
     {
       name: "projects",
       path: "/projects",
       element: Home,
-      desc: "some of my projects"
+      desc: "some of my projects",
     },
     {
       name: "contact",
       path: "/contact",
       element: Home,
-      desc: "get in touch with me"
+      desc: "get in touch with me",
     },
   ];
+  const location = useLocation();
   return (
     <>
       <Navbar routes={routes} />
-      <Routes>
-        {routes.map((route) => (
-          <Route path={route.path} element={<route.element routes={routes} />} key={route.path} />
-        ))}
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          {routes.map((route) => (
+            <Route
+              index={route.path === "/"}
+              path={route.path}
+              element={<route.element routes={routes} />}
+              key={route.path}
+            />
+          ))}
+        </Routes>
+      </AnimatePresence>
     </>
   );
 };
