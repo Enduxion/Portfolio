5e4e24fd3a4c744918f79c91a033a2c3ec6d314d
Enduxion <endux.pg@gmail.com>
2024-08-01 13:02:36 +0545

    Added responsiveness to the projectId page.!

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -5,7 +5,7 @@ const ProjectCard = forwardRef(({ project, setPid, isCurrentlySelected }, ref) =
   return (
     <div
       ref={ref}
-      className={`w-1/3 flex flex-col shrink-0 h-fit bg-gradient-to-tr from-primary-4/10 to-primary-4/20 p-4 rounded-3xl gap-2 cursor-pointer border-2 ${isCurrentlySelected ? "border-primary-3" : "border-primary-4/20"}`}
+      className={`lg:w-1/3 sm:w-10/12 flex flex-col shrink-0 h-fit bg-gradient-to-tr from-primary-2/70 to-primary-2/90 p-4 rounded-3xl backdrop-blur-md gap-2 cursor-pointer border-2 ${isCurrentlySelected ? "border-primary-3/50" : "border-primary-4/20"}`}
       id={project.name}
       onClick={setPid}
     >
--- src/pages/ProjectId/ProjectId.jsx
+++ src/pages/ProjectId/ProjectId.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
-import { projectArr } from "../../resources/utils";
+import { bga, projectArr } from "../../resources/utils";
 import Transition from "../../functions/Transition";
 import twc from "../../functions/twc";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
+import VideoComponent from "../../components/VideoComponent/VIdeoComponent";
 
 const ProjectId = () => {
   const { id } = useParams();
@@ -24,13 +25,17 @@ const ProjectId = () => {
 
   return (
     <div className="section py-8 flex flex-col">
+      <VideoComponent src={bga} />
       <div className="flex flex-col">
         <span className="font-bold text-xl">
           Projects are key to your development
         </span>
         <span className="text-primary-4">Here are some of my projects!</span>
+        {projectArr.find((project) => project.name.toLowerCase() === id.toLowerCase()) === undefined && (
+          <span className="text-primary-6">*'{id}' not found</span>
+        )}
       </div>
-      <div className="hidden-scrollbar flex flex-row overflow-x-scroll grow items-center gap-4">
+      <div className="hidden-scrollbar flex flex-row overflow-x-scroll grow items-center gap-8">
         {projectArr.map((project, index) => (
           <ProjectCard
             ref={(el) => (projectRefs.current[index] = el)}
