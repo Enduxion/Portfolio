696bd9e0d7733dd380e7cff54581a9abf731e4dd
Enduxion <endux.pg@gmail.com>
2024-08-05 11:45:01 +0545

    *fixed bug made contact page take full width in large devices

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import OtherContact from "./OtherContact/OtherContact";
 
 const Contact = () => {
   return (
-    <div className="lg:section sm:px-4 sm:mt-nav sm:h-auto sm:tracking-widest sm:w-full flex flex-col items-center justify-center gap-2 lg:py-0 sm:py-8">
+    <div className="lg:section sm:px-4 sm:mt-nav lg:h-anav sm:h-auto tracking-widest sm:w-full flex flex-col items-center justify-center gap-2 lg:py-0 sm:py-8">
       <VideoComponent src={bgc} />
       <div className="flex lg:flex-row sm:flex-col w-full h-full lg:justify-between sm:justify-start items-center gap-16">
         <Direct />
